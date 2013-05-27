99e6e9bf314a45faeb835ac27e59be9d2f77c530
Marco Krikke <marco@eveoh.nl>
2013-05-27 22:15:01 +0200

    Added recent blogs to header.

---
+++ content/assets/js/eveoh.js
@@ -0,0 +1,15 @@
+// Callback for blog request
+var loadPosts = function (data) {
+    var baseurl = 'http://blog.eveoh.nl';
+    var header = '<h1><a href="http://blog.eveoh.nl">Recent blog posts</a></h1>';
+    var content = '<ul>';
+
+    for (var i = 0; i < data.length; i++) {
+        content += '<li><a class="title" href="' + baseurl + data[i].url + '">' + data[i].title + '</a> <small>[' + data[i].date + ']</small></li>';
+    }
+
+    content += '</ul>';
+
+    document.getElementById('blog').innerHTML = header + content;
+    document.getElementById('blog-mobile').innerHTML = content;
+}
